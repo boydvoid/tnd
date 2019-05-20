ee97e93f0fe4cb10ee03dff0d36a5440c701dd23
Bobby Boyd <bboyd2008@gmail.com>
2019-05-19 21:37:39 -0700

    testing megadraft

--- client/src/Components/BlogEditor/BlogEditor.js
+++ client/src/Components/BlogEditor/BlogEditor.js
@@ -9,11 +9,12 @@ import Input from '../Input/Input.js'
 import _ from "lodash"
 import PBtn from '../PBtn/PBtn';
 import api from '../../utils/api';
-
+import { MegadraftEditor, editorStateFromRaw } from "megadraft";
+import '../../../node_modules/megadraft/dist/css/megadraft.css'
 const BlogEditor = (props) => {
 
-  const [editorState,setEditorState ] = useState(EditorState.createEmpty())
-  const [editorHTML,setEditorHTML ] = useState({__html: '<div></div>'})
+  const [editorState, setEditorState] = useState(EditorState.createEmpty())
+  const [editorHTML, setEditorHTML] = useState({ __html: '<div></div>' })
   const [titleInputVal, setTitleInputVal] = useState('')
   const [id, setId] = useState('')
   const [imageurl, setImageurl] = useState('')
@@ -32,11 +33,11 @@ const BlogEditor = (props) => {
       setLive(blog.data.live)
 
       const blocksFromHTML = htmlToDraft(blog.data.blog);
-      const {contentBlocks, entityMap } = blocksFromHTML;
-      const contentState= ContentState.createFromBlockArray(contentBlocks, entityMap);
+      const { contentBlocks, entityMap } = blocksFromHTML;
+      const contentState = ContentState.createFromBlockArray(contentBlocks, entityMap);
 
       setEditorState(EditorState.createWithContent(contentState))
-      setEditorHTML({__html: blog.data.blog})
+      setEditorHTML({ __html: blog.data.blog })
     })
 
     return () => {
@@ -47,7 +48,7 @@ const BlogEditor = (props) => {
   const onEditorStateChange = (editorState) => {
 
     setEditorState(editorState)
-    setEditorHTML({__html: draftToHtml(convertToRaw(editorState.getCurrentContent()))})
+    setEditorHTML({ __html: draftToHtml(convertToRaw(editorState.getCurrentContent())) })
     //save on update
     //need to debounce to save when youre done typing
     //need to test w/ localStorage
@@ -56,7 +57,7 @@ const BlogEditor = (props) => {
   const toggleLive = () => {
     let change;
 
-    if(live) {
+    if (live) {
       change = false
     } else {
       change = true
@@ -77,52 +78,55 @@ const BlogEditor = (props) => {
 
   const save = () => {
     console.log('run save')
-   // save to db
-   let data = {
-     username: props.username,
-     blog: draftToHtml(convertToRaw(editorState.getCurrentContent())),
-     title: titleInputVal,
-     id: id,
-     img: imageurl,
-     live: live
-   }
-   api.saveBlog(data).then(res => {
-     console.log(res)
-   })
+    // save to db
+    let data = {
+      username: props.username,
+      blog: draftToHtml(convertToRaw(editorState.getCurrentContent())),
+      title: titleInputVal,
+      id: id,
+      img: imageurl,
+      live: live
+    }
+    api.saveBlog(data).then(res => {
+      console.log(res)
+    })
   }
 
   const handleChange = event => {
-    if(event.target.name === 'titleInputVal'){
+    if (event.target.name === 'titleInputVal') {
       setTitleInputVal(event.target.value)
     }
 
-    if(event.target.name === 'imageurl'){
+    if (event.target.name === 'imageurl') {
       setImageurl(event.target.value)
     }
   }
 
-    return (
-      <div className="editorContent">
-        <div class="title">
-          <Input className="title-box" placeholder="Title"onChange={handleChange} name="titleInputVal" value={titleInputVal}/>
-        </div>
-        <div className="editorWrapper">
-          <Editor
+  return (
+    <div className="editorContent">
+      <div class="title">
+        <Input className="title-box" placeholder="Title" onChange={handleChange} name="titleInputVal" value={titleInputVal} />
+      </div>
+      <div className="editorWrapper">
+        {/* <Editor
             editorState={editorState}
             onEditorStateChange={onEditorStateChange}
             toolbarClassName="toolbar-class"
-          />
-          <div>
-            <Input placeholder="Image URL" className="img-input" value={imageurl} name="imageurl" onChange={handleChange}/>
-            <PBtn onClick={save}>Save</PBtn>
-            <PBtn onClick={toggleLive}>Toggle Live</PBtn>
-          </div>
-        </div>
-        {/*Preview div*/}
-        <div className="preview">
-          <span dangerouslySetInnerHTML={editorHTML} />
+          /> */}
+        <MegadraftEditor
+          editorState={editorState}
+          onChange={onEditorStateChange} />
+        <div>
+          <Input placeholder="Image URL" className="img-input" value={imageurl} name="imageurl" onChange={handleChange} />
+          <PBtn onClick={save}>Save</PBtn>
+          <PBtn onClick={toggleLive}>Toggle Live</PBtn>
         </div>
       </div>
-    );
-  }
+      {/*Preview div*/}
+      <div className="preview">
+        <span dangerouslySetInnerHTML={editorHTML} />
+      </div>
+    </div>
+  );
+}
 export default BlogEditor;
--- client/src/Pages/BlogPage.js
+++ client/src/Pages/BlogPage.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import IconNav from '../Components/IconNav/IconNav';
 import api from '../utils/api';
 import { EditorState, convertToRaw, convertFromRaw, ContentState } from 'draft-js';
@@ -8,75 +8,84 @@ import htmlToDraft from 'html-to-draftjs';
 import Navbar from '../Components/Navbar/Navbar';
 import PBtn from '../Components/PBtn/PBtn'
 import Input from '../Components/Input/Input'
+import './BlogPage.css'
 const BlogPage = (props) => {
 	const [id, setId] = useState('')
-	const [editorHTML, setEditorHTML] = useState({__html: '<div></div>'})
+	const [editorHTML, setEditorHTML] = useState({ __html: '<div></div>' })
 	const [titleInputVal, setTitleInputVal] = useState('')
 	const [date, setDate] = useState('')
-	const [imageUrl, setImageUrl]	 = useState('')
+	const [imageUrl, setImageUrl] = useState('')
 
 
 	useEffect(() => {
 
-    let url = window.location.href.split('/'); 
+		let url = window.location.href.split('/');
 
 		setId(url[4])
 		api.loadBlog(url[4]).then(blog => {
 			let viewsUpdate = {
-        id: url[4],
-        views: blog.data.views + 1
-      }
+				id: url[4],
+				views: blog.data.views + 1
+			}
 
-      api.updateViews(viewsUpdate).then(done => {
-        console.log('updated views')
-      })
-			
+			api.updateViews(viewsUpdate).then(done => {
+				console.log('updated views')
+			})
+
 			setTitleInputVal(blog.data.title)
 			setImageUrl(blog.data.img)
 			setDate(blog.data.date)
 
-      const blocksFromHTML = htmlToDraft(blog.data.blog);
-      const {contentBlocks, entityMap } = blocksFromHTML;
-			const contentState= ContentState.createFromBlockArray(contentBlocks, entityMap);
-			
-			setEditorHTML({__html: blog.data.blog})
-    })
-		
-	}, [])
-	
+			const blocksFromHTML = htmlToDraft(blog.data.blog);
+			const { contentBlocks, entityMap } = blocksFromHTML;
+			const contentState = ContentState.createFromBlockArray(contentBlocks, entityMap);
 
-		return (
-			<div className="blogPage">
-				<Navbar position='right'>
-					<ul>
-						<li> <PBtn link="/" external={false}>Home</PBtn> </li>
-						<li> <PBtn link="/my-blog"external={false}>Blog</PBtn> </li>
-						<li> <PBtn link="https://www.teacherspayteachers.com/Store/The-Teacher-Next-Door" external={true}>My Store</PBtn> </li>
-						<li> <PBtn link="/teacher-freebies"external={false}>Teacher Freebies</PBtn> </li>
-						<li> <PBtn link="/meet-jenn"external={false}>Meet Jenn</PBtn> </li>
-						<li> <PBtn link="/contact-me"external={false}>Contact Me</PBtn> </li>
-						<li> <PBtn link="https://www.facebook.com/TheTeacherNextDoor" external={true}><i className="fab fa-facebook" ></i></PBtn> </li>
-						<li> <PBtn link="https://www.instagram.com/theteachernextdoor/" external={true}><i className="fab fa-instagram" ></i></PBtn> </li>
-						<li> <PBtn link="https://www.pinterest.com/TeacherNextDoor/" external={true}><i className="fab fa-pinterest-square" ></i></PBtn> </li>
-						<li> <PBtn link="https://www.teacherspayteachers.com/Store/The-Teacher-Next-Door" external={true}>TpT</PBtn> </li>
-						<li><Input className="searchBar" type="text" placeholder="Search..."/></li>
-					</ul>	
-				</Navbar>
-				<IconNav/>
-				<div className="container">
-					<div className="row">
-						<div className="col-xl-12 center">
-							<h1>{titleInputVal}</h1>
-							<p>By: Jennifer Larson</p>	
+			setEditorHTML({ __html: blog.data.blog })
+		})
+
+	}, [])
+
+
+	return (
+		<div className="blogPage">
+			<Navbar position='right'>
+				<ul>
+					<li> <PBtn link="/" external={false}>Home</PBtn> </li>
+					<li> <PBtn link="/my-blog" external={false}>Blog</PBtn> </li>
+					<li> <PBtn link="https://www.teacherspayteachers.com/Store/The-Teacher-Next-Door" external={true}>My Store</PBtn> </li>
+					<li> <PBtn link="/teacher-freebies" external={false}>Teacher Freebies</PBtn> </li>
+					<li> <PBtn link="/meet-jenn" external={false}>Meet Jenn</PBtn> </li>
+					<li> <PBtn link="/contact-me" external={false}>Contact Me</PBtn> </li>
+					<li> <PBtn link="https://www.facebook.com/TheTeacherNextDoor" external={true}><i className="fab fa-facebook" ></i></PBtn> </li>
+					<li> <PBtn link="https://www.instagram.com/theteachernextdoor/" external={true}><i className="fab fa-instagram" ></i></PBtn> </li>
+					<li> <PBtn link="https://www.pinterest.com/TeacherNextDoor/" external={true}><i className="fab fa-pinterest-square" ></i></PBtn> </li>
+					<li> <PBtn link="https://www.teacherspayteachers.com/Store/The-Teacher-Next-Door" external={true}>TpT</PBtn> </li>
+					<li><Input className="searchBar" type="text" placeholder="Search..." /></li>
+				</ul>
+			</Navbar>
+			<IconNav />
+			<div className="container-fluid blog-container">
+				<div className="row">
+					<div className="col-xl-12 center">
+						<div className="title-container">
+							<h1 className="blog-title">{titleInputVal}</h1>
+							<p>By: Jennifer Larson</p>
 							<p>{date}</p>
-							<div className="preview">
-								<span dangerouslySetInnerHTML={editorHTML} />
-							</div>
-						</div>	
-					</div>	
+							<ul>
+								<li> <PBtn link="https://www.facebook.com/TheTeacherNextDoor" external={true}><i className="fab fa-facebook" ></i></PBtn> </li>
+								<li> <PBtn link="https://www.instagram.com/theteachernextdoor/" external={true}><i className="fab fa-instagram" ></i></PBtn> </li>
+								<li> <PBtn link="https://www.pinterest.com/TeacherNextDoor/" external={true}><i className="fab fa-pinterest-square" ></i></PBtn> </li>
+								<li> <PBtn link="https://www.teacherspayteachers.com/Store/The-Teacher-Next-Door" external={true}>TpT</PBtn> </li>
+							</ul>
+						</div>
+						<div className="preview">
+							<span dangerouslySetInnerHTML={editorHTML} />
+						</div>
+					</div>
 				</div>
 			</div>
-		)
-	}	
+		</div>
+	)
+}
 
 export default BlogPage;

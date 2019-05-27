a29ff2ad9d6ac26c00bd8ecb51d1f1f6015c14ab
Bobby Boyd <bboyd2008@gmail.com>
2019-05-27 10:08:15 -0700

    'slider'

--- client/src/Components/CollectionSlider/CollectionSlider.js
+++ client/src/Components/CollectionSlider/CollectionSlider.js
@@ -1,25 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PBtn from '../PBtn/PBtn';
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
-class CollectionSlider extends Component {
-	state={
-		images: ['https://ecdn.teacherspayteachers.com/thumbitem/Nonfiction-Reading-Games-Reading-Centers-3244260-1547399353/original-3244260-1.jpg', 'https://ecdn.teacherspayteachers.com/thumbitem/Nonfiction-Reading-Games-Reading-Centers-3244260-1547399353/original-3244260-1.jpg', 'https://ecdn.teacherspayteachers.com/thumbitem/Nonfiction-Reading-Games-Reading-Centers-3244260-1547399353/original-3244260-1.jpg', 'https://ecdn.teacherspayteachers.com/thumbitem/Nonfiction-Reading-Games-Reading-Centers-3244260-1547399353/original-3244260-1.jpg', 'https://ecdn.teacherspayteachers.com/thumbitem/Nonfiction-Reading-Games-Reading-Centers-3244260-1547399353/original-3244260-1.jpg'], 
-		links: [
-			'https://www.teacherspayteachers.com/Product/Inferences-Using-Literature-for-3rd-5th-Grades-935588',
-			'https://www.teacherspayteachers.com/Product/Cause-and-Effect-Informational-Text-3rd-Grade-Cause-and-Effect-1136395',
-			'https://www.teacherspayteachers.com/Product/Inferences-Using-Informational-Text-4th-and-5th-Grade-Inferences-855807',
-			'https://www.teacherspayteachers.com/Product/Nonfiction-Reading-Games-Bundle-for-4th-and-5th-Grades-3244260'
-		
-		
-		]
-	}
-	render () {
-		return (
-			<div className="container-fluid grey">
-		
+import './CollectionSlider.css'
+const CollectionSlider = (props) => {
+
+	const [items, setItems] = useState([
+		{ img: 'https://ecdn.teacherspayteachers.com/thumbitem/Nonfiction-Reading-Games-Reading-Centers-3244260-1547399353/original-3244260-1.jpg', link: 'https://www.teacherspayteachers.com/Product/Inferences-Using-Literature-for-3rd-5th-Grades-935588' },
+		{ img: 'https://ecdn.teacherspayteachers.com/thumbitem/Nonfiction-Reading-Games-Reading-Centers-3244260-1547399353/original-3244260-1.jpg', link: 'https://www.teacherspayteachers.com/Product/Inferences-Using-Literature-for-3rd-5th-Grades-935588' },
+		{ img: 'https://ecdn.teacherspayteachers.com/thumbitem/Nonfiction-Reading-Games-Reading-Centers-3244260-1547399353/original-3244260-1.jpg', link: 'https://www.teacherspayteachers.com/Product/Inferences-Using-Literature-for-3rd-5th-Grades-935588' },
+		{ img: 'https://ecdn.teacherspayteachers.com/thumbitem/Nonfiction-Reading-Games-Reading-Centers-3244260-1547399353/original-3244260-1.jpg', link: 'https://www.teacherspayteachers.com/Product/Inferences-Using-Literature-for-3rd-5th-Grades-935588' },
+		{ img: 'https://ecdn.teacherspayteachers.com/thumbitem/Nonfiction-Reading-Games-Reading-Centers-3244260-1547399353/original-3244260-1.jpg', link: 'https://www.teacherspayteachers.com/Product/Inferences-Using-Literature-for-3rd-5th-Grades-935588' },
+		{ img: 'https://ecdn.teacherspayteachers.com/thumbitem/Nonfiction-Reading-Games-Reading-Centers-3244260-1547399353/original-3244260-1.jpg', link: 'https://www.teacherspayteachers.com/Product/Inferences-Using-Literature-for-3rd-5th-Grades-935588' },
+	])
+
+	return (
+		<div className="container-fluid grey">
+			<div className="slider">
+				{
+					items.map((item, index) => {
+						return (
+							<div className="slider-item" key={index}>
+								<img src={item.img} alt="" />
+								<a href={item.link}>Link</a>
+							</div>
+						)
+					})
+				}
 			</div>
-		)
-	}
+		</div>
+	)
 }
 
 export default CollectionSlider;
